52cec32356cfbf75d318de75c788d5938b9b613d
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-04-22 20:58:27 +0200

    chore: update App component structure and styling for ToDoApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className="font-sans">
-      <div className="text-4xl">Test</div>
+    <div className="font-sans bg-[#f1d4b3] min-h-screen">
+      <h1 className="text-[120px] font-bold text-black/5 uppercase absolute left-1/2 -translate-x-1/2">
+        ToDoApp
+      </h1>
     </div>
   );
 }
