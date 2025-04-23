51dd245b5039e05e3895cfb0540dd4051abd1f41
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-04-23 22:03:44 +0200

    feat: add bg and main section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 function App() {
   return (
-    <div className="font-sans bg-[#f1d4b3] min-h-screen">
-      <h1 className="text-[120px] font-bold text-black/5 uppercase absolute left-1/2 -translate-x-1/2">
+    <div className="flex justify-center items-center font-sans bg-[#f1d4b3] min-h-screen">
+      <h1 className="text-[120px] font-bold text-black/5 uppercase absolute left-1/2 -translate-x-1/2 tracking-[0.2em]">
         ToDoApp
       </h1>
+
+      <main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08)]">
+        <header></header>
+        <ul></ul>
+        <section></section>
+      </main>
+
+      <footer></footer>
     </div>
   );
 }
