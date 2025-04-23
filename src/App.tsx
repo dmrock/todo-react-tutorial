a349aa3799caf26d5ab4709614f2071925d1477a
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-04-23 22:46:21 +0200

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="flex justify-center items-center font-sans bg-[#f1d4b3] min-h-screen">
+    <div className="flex flex-col justify-center items-center font-sans bg-[#f1d4b3] min-h-screen">
       <h1 className="text-[120px] font-bold text-black/5 uppercase absolute left-1/2 -translate-x-1/2 tracking-[0.2em]">
         ToDoApp
       </h1>
@@ -11,7 +11,12 @@ function App() {
         <section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[0.08]"></section>
       </main>
 
-      <footer></footer>
+      <footer className="flex justify-between w-[972px] items-center text-[11px] opacity-30 mt-[12px]">
+        <small className="text-[11px]">&copy; 2025. Copyright by dmrock.</small>
+        <p>
+          Version <b>0.1</b> (beta)
+        </p>
+      </footer>
     </div>
   );
 }
