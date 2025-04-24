f81c5ad0c03d15e7967475c7f1a793d1c78c520d
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-04-24 17:07:24 +0200

    feat: refactor App component to use BackgroundHeading and improve structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import BackgroundHeading from './components/BackgroundHeading';
+
 function App() {
   return (
     <div className="flex flex-col justify-center items-center font-sans bg-[#f1d4b3] min-h-screen">
-      <h1 className="text-[120px] font-bold text-black/5 uppercase absolute left-1/2 -translate-x-1/2 tracking-[0.2em]">
-        ToDoApp
-      </h1>
+      <BackgroundHeading />
 
       <main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08)] grid grid-cols-[7fr_4fr] grid-rows-[59px_1fr] overflow-hidden">
         <header className="col-[1/3] row-[1/2] bg-[#fbf5ed] border-b border-black/[0.08]"></header>
