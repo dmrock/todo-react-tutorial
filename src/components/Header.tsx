169b362e55a18d959e243d8ecf6d67c316d70212
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-04-25 17:39:09 +0200

    feat: enhance Header component with layout and todos count display

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
 export default function Header() {
   return (
-    <header className="col-[1/3] row-[1/2] bg-[#fbf5ed] border-b border-black/[0.08]"></header>
+    <header className="flex justify-between items-center px-[28px] col-[1/3] row-[1/2] bg-[#fbf5ed] border-b border-black/[0.08]">
+      <img src="https://bytegrad.com/course-assets/react-nextjs/dots.png" alt="dots" />
+
+      <p>
+        <b>0</b> / 0 todos completed
+      </p>
+    </header>
   );
 }
