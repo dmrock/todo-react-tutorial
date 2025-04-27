ab0fddfc424c27921261fd7eda42f6052274c585
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-04-27 14:36:22 +0200

    feat: add AddTodoForm component and integrate it into Sidebar

---
+++ src/components/AddTodoForm.tsx
@@ -0,0 +1,16 @@
+export default function AddTodoForm() {
+  return (
+    <form>
+      <h2 className="font-medium text-[#231d15]">Add a todo</h2>
+      <input
+        type="text"
+        className="h-[45px] border border-black/[12%] rounded-[5px] my-[9px] text-[14px] block w-full px-[16px]"
+      />
+      <button
+        type="submit"
+        className="h-[45px] bg-[#473a2b] hover:bg-[#322618] w-full text-white rounded-[5px] cursor-pointer">
+        Add
+      </button>
+    </form>
+  );
+}
