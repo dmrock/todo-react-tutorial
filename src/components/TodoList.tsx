70cf894e3e1e10680276c3ae1ad4436ea1fee211
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-04-27 19:11:56 +0200

    feat: implement TodoList component to display initial todos

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,16 @@
+const initialTodos = ['Learn React', 'Learn TypeScript', 'Learn Tailwind CSS', 'Learn Next.js'];
+
 export default function TodoList() {
-  return <ul></ul>;
+  return (
+    <ul>
+      {initialTodos.map((todo) => (
+        <li
+          key={todo}
+          className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-black/[8%]">
+          <span>{todo}</span>
+          <button>❌</button>
+        </li>
+      ))}
+    </ul>
+  );
 }
