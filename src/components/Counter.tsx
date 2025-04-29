fbb4e202db29e95db2d5cfbd3d4287517bbec401
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-04-29 21:42:23 +0200

    feat: update Counter component to display dynamic count of completed todos

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
+
 export default function Counter() {
+  const [count, setCount] = useState(0);
+
   return (
     <p>
-      <b>0</b> / 0 todos completed
+      <b>{count}</b> / 0 todos completed
     </p>
   );
 }
