1b92d9811e2ae333bf0c49f8f550ce35abecad08
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-04-29 21:33:46 +0200

    feat: replace initialTodos with state management in TodoList component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,15 +1,16 @@
+import { useState } from 'react';
 import DeleteButton from './DeleteButton';
 
-const initialTodos = [
-  { text: 'Learn React', isCompleted: false },
-  { text: 'Learn TypeScript', isCompleted: true },
-  { text: 'Learn Tailwind CSS', isCompleted: false },
-];
-
 export default function TodoList() {
+  const [todos, setTodos] = useState([
+    { text: 'Learn React', isCompleted: false },
+    { text: 'Learn TypeScript', isCompleted: true },
+    { text: 'Learn Tailwind CSS', isCompleted: false },
+  ]);
+
   return (
     <ul>
-      {initialTodos.map((todo) => (
+      {todos.map((todo) => (
         <li
           key={todo.text}
           className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-black/[8%]">
