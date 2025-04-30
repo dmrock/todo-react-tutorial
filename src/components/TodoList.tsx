fbe0b3c7295841e3f075af485d4f267e4ee4c64b
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-04-30 19:28:33 +0200

    feat: update TodoList to include unique IDs for todos and fix key prop

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,17 +3,27 @@ import DeleteButton from './DeleteButton';
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
-    { text: 'Learn React', isCompleted: false },
-    { text: 'Learn TypeScript', isCompleted: true },
-    { text: 'Learn Tailwind CSS', isCompleted: false },
+    { id: 1, text: 'Learn React', isCompleted: false },
+    { id: 2, text: 'Learn TypeScript', isCompleted: false },
+    { id: 3, text: 'Learn Tailwind CSS', isCompleted: false },
   ]);
 
   return (
     <ul>
       {todos.map((todo) => (
         <li
-          key={todo.text}
-          className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-black/[8%]">
+          key={todo.id}
+          className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-black/[8%]"
+          onClick={() => {
+            setTodos(
+              todos.map((todo) => {
+                return {
+                  ...todo,
+                  isCompleted: todo.id === todo.id ? !todo.isCompleted : todo.isCompleted,
+                };
+              }),
+            );
+          }}>
           <span className={`${todo.isCompleted ? 'line-through text-[#ccc]' : ''}`}>
             {todo.text}
           </span>
