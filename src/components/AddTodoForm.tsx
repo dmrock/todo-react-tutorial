64ae0f6578911df3481f1a19d8b1b62e31eec47b
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-05-02 22:37:26 +0200

    feat: update AddTodoForm and Sidebar components to manage todos state via props

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import Button from './Button';
 
-export default function AddTodoForm() {
+export default function AddTodoForm({ todos, setTodos }) {
+  const [todoText, setTodoText] = useState('');
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        setTodos((prev) => [
+          ...prev,
+          {
+            id: prev.length + 1,
+            text: todoText,
+            isCompleted: false,
+          },
+        ]);
+        setTodoText('');
+      }}>
       <h2 className="font-medium text-[#231d15]">Add a todo</h2>
       <input
         type="text"
         className="h-[45px] border border-black/[12%] rounded-[5px] my-[9px] text-[14px] block w-full px-[16px]"
+        value={todoText}
+        onChange={(e) => setTodoText(e.target.value)}
       />
       <Button>Add</Button>
     </form>
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,16 +12,14 @@ function App() {
     { id: 3, text: 'Learn Tailwind CSS', isCompleted: false },
   ]);
 
-  const totalNumberOfTodos = todos.length;
-
   return (
     <div className="flex flex-col justify-center items-center font-sans bg-[#f1d4b3] min-h-screen">
       <BackgroundHeading />
 
       <main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08)] grid grid-cols-[7fr_4fr] grid-rows-[59px_1fr] overflow-hidden">
-        <Header todos={todos} setTodos={setTodos} />
+        <Header todos={todos} />
         <TodoList todos={todos} setTodos={setTodos} />
-        <Sidebar />
+        <Sidebar todos={todos} setTodos={setTodos} />
       </main>
       <Footer />
     </div>
